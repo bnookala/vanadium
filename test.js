0a00c0fac2c8feb16a68644a5a83067379494b62
Bhargav Nookala <nooknb@gmail.com>
2012-06-07 01:40:07 -0700

    Updating test

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 var request = require('request');
+var vanadium = require('./vanadium');
+
+console.log(vanadium);
+var test = new vanadium();
+test.test();
 
 request('http://localhost:9000/json', function (err, response, body) {
 	console.log(body);
