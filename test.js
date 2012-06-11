a5c2698f590f7a1e25d0e89d8fe7d61a3c7b0d1e
Bhargav Nookala <nooknb@gmail.com>
2012-06-11 01:24:58 -0700

    It works

--- test.js
+++ test.js
@@ -1,10 +1,41 @@
 var request = require('request');
-var vanadium = require('./vanadium');
+var WebSocketClient = require('websocket').client;
+//var vanadium = require('./vanadium');
 
-console.log(vanadium);
-var test = new vanadium();
-test.test();
+//console.log(vanadium);
+//var test = new vanadium.Vanadium();
+//test.test();
 
 request('http://localhost:9000/json', function (err, response, body) {
-	console.log(body);
+	var response = JSON.parse(body);
+	var ws_uri = response[0]['webSocketDebuggerUrl'];
+	var socket = new WebSocketClient();
+	socket.connect('ws://localhost:9000/devtools/page/21_1');
+	socket.on('connect', function (connection) {
+		console.log('connected');
+
+		connection.on('message', function (message) {
+			console.log(message);
+		});
+
+		connection.on('error', function () {
+			console.log(error);
+		});
+
+		function sendTestPackage () {
+			var json_pkg = {
+				"id": 1,
+				"method": "Runtime.evaluate",
+				"params": {
+					"expression": "alert('omg');",
+				},
+			};
+
+			var json_string = JSON.stringify(json_pkg);
+			console.log(json_pkg);
+			connection.sendUTF(json_string);
+		};
+
+		sendTestPackage();
+	});
 });
